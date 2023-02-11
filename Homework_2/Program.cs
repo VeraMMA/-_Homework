84ff3dd88c6d9d647f1a78945033c2f8e3d339cd
Vera Maria <mymadonna89@mail.ru>
2023-02-11 22:12:55 +0600

    в Homework2 решен первый пункт задачи №2

---
+++ Homework_2/Program.cs
@@ -0,0 +1,26 @@
+﻿
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.WriteLine("Найти максимальное из трех чисел");
+Console.WriteLine("Введите одно из трех чисел: 2, 3, 7");
+
+string number = Console.ReadLine();
+
+if (number == "7")
+{
+    Console.WriteLine("Это максимальное число");
+}
+if (number == "3")
+{
+    Console.WriteLine("Это среднее число");
+}
+if (number == "2")
+{
+    Console.WriteLine("Это минимальное число");
+}
+
+
+
+
