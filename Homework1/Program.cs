4453cffa71519615072fa12f3492586a6a8ded1b
Vera Maria <mymadonna89@mail.ru>
2023-02-11 20:43:21 +0600

    initial commit

---
+++ Homework1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("2. Даны два числа. Показать большее и меньшее число");
+
+int a = Console.WriteLine;
+int b = Console.WriteLine;
+
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
