665d18e8f575e0e688340e20a14e67c84927e5cb
Vera Maria <mymadonna89@mail.ru>
2023-02-11 21:11:43 +0600

    в Homework1 решен первый пункт задачи №1

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -1,6 +1,15 @@
 ﻿Console.WriteLine("Задача 1. Даны два числа. Показать большее и меньшее число");
 
-//a = 5; b = 7 -> max = 7
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
 
+int a = 5;
+int b = 7;
+
+if (a > b)
+
+    Console.WriteLine("max = " + a);
+
+if (b > a)
+
+    Console.WriteLine("max = " + b);
