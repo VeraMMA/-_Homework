a56d8d469f2fc01a4c6bb416e25a89f54b290c26
Vera Maria <mymadonna89@mail.ru>
2023-02-13 16:31:47 +0600

    Все задачи решены

---
+++ Homework_4/Homework_4-1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("Показать все чётные числа от 1 до N.");
+
+        Console.WriteLine("Введите число");
+
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        int t = 1;
+        while (t <= N)
+        {
+            Console.WriteLine(t);
+            t = t + 1;
+        }
+        Console.WriteLine("Четными будут: ");
+        t = 1;
+        while (t <= N)
+        {
+            if (t % 2 == 0)
+            {
+                Console.WriteLine(t);
+            }
+            t = t + 1;
+        }
