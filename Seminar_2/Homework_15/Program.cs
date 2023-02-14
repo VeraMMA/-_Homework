803a79691efe8dbdb3e06add6c8a18fc10fd3beb
Vera Maria <mymadonna89@mail.ru>
2023-02-14 21:31:26 +0600

    В Задаче 15 добавлено новое решена

--- Seminar_2/Homework_15/Program.cs
+++ Seminar_2/Homework_15/Program.cs
@@ -2,10 +2,15 @@
 
 int day = 7;
 
-string[] days = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+string[] days = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
 
-    Console.WriteLine(days[day - 1]);
-if(day == 6 || day == 7)
+Console.WriteLine(days[day - 1]);
+
+if (day >= 1 && day <= 5)
+{
+    Console.WriteLine("Будний день");
+}
+if (day == 6 || day == 7)
 {
     Console.WriteLine("Выходной");
 }
