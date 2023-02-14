af41a4ab726ec846e4024781468966eae26cf576
Vera Maria <mymadonna89@mail.ru>
2023-02-14 18:53:44 +0600

    New files added

---
+++ Sem2/Program.cs
@@ -0,0 +1,17 @@
+﻿ {
+        Console.WriteLine("Выяснить, кратно ли число заданному, если нет, вывести остаток.");
+
+       
+        Console.WriteLine("Введите первое число ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите второе число ");
+         int b = Convert.ToInt32(Console.ReadLine());
+   
+        if (a % b == 0)
+        {
+            Console.WriteLine("Числа кратные");
+        }
+        else Console.WriteLine(a % b);
+        
+    }
+    
