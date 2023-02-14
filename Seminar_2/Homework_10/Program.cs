766d0993b677d03c011fd64e6cd31d9d3f02abf9
Vera Maria <mymadonna89@mail.ru>
2023-02-14 20:43:54 +0600

    Seminar_2 added

---
+++ Seminar_2/Homework_10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите трезначное число:");
+
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if ( numberA / 100 > 1)
+{
+    numberA = numberA / 10 % 10;
+}
+Console.WriteLine(numberA);
---
+++ Seminar_2/Homework_13/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Вывести третью цифру заданного числа или сообщить, что третьей цифры нет.");
+Console.WriteLine("Введите число:");
+
+int numberX = Convert.ToInt32(Console.ReadLine());
+
+if (numberX > 1000)
+{
+    numberX = numberX / 10 % 10;
+    Console.WriteLine(numberX);
+}
+
+if (numberX < 1000 && numberX > 100)
+{
+    numberX = numberX % 10;
+    Console.WriteLine(numberX);
+}
+if (numberX < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
