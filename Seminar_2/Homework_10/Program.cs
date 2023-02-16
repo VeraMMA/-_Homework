2cc8e9e7c73d800d188fe2263c2148c3b241be56
Vera Maria <mymadonna89@mail.ru>
2023-02-16 19:51:45 +0600

    Seminar 2 changed again

--- Seminar_2/Homework_10/Program.cs
+++ Seminar_2/Homework_10/Program.cs
@@ -5,20 +5,20 @@ namespace homework
     {
         static void Main()
         {
-           Finder(000);
+           Finder(567);
         }
         public static void Print(string word){
             Console.WriteLine(word);
         }
+          
         public static void Finder(int numberA)
         {
             Print("Введите трезначное число:");
             numberA = Convert.ToInt32(Console.ReadLine());
-
-            if (numberA > 99 && numberA < 1000)
-            {
+            bool treeDigit = numberA > 99 && numberA < 1000;
+            
                 numberA = numberA / 10 % 10;
-            }
+    
             Console.WriteLine(numberA);
         }
     }
