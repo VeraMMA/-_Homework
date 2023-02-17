98918b659f4e7cd12038c9bbcaa91f31a428efbb
Vera Maria <mymadonna89@mail.ru>
2023-02-17 21:08:31 +0600

    Добавлены решения задач с использованием методов

--- Seminar_2/Homework_10/Program.cs
+++ Seminar_2/Homework_10/Program.cs
@@ -5,23 +5,23 @@ namespace homework
     {
         static void Main()
         {
-           Finder(567);
+           Print("Введите трезначное число:");
+           treeDigit(456);
         }
         public static void Print(string word){
             Console.WriteLine(word);
         }
           
-        public static void Finder(int numberA)
+        static void treeDigit (int numberA)
         {
-            Print("Введите трезначное число:");
-            numberA = Convert.ToInt32(Console.ReadLine());
-            bool treeDigit = numberA > 99 && numberA < 1000;
             
-                numberA = numberA / 10 % 10;
-    
+            if(numberA > 99 && numberA < 1000)
+              numberA = numberA / 10 % 10;
             Console.WriteLine(numberA);
+
+        } 
+            
         }
-    }
 
 }
 
